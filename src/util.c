c7cbf83faff6f1e51988d647418044d4ab11c720
Vighnesh Birodkar <vighneshbirodkar@gmail.com>
2014-02-03 03:25:42 +0530

    List done,put murmurhash3

---
+++ src/util.c
@@ -0,0 +1,20 @@
+#include "util.h"
+#include<stdlib.h>
+
+/* 
+ * DEBAGGING
+*/
+void gen_random(char *s,const int len) 
+{
+    static const char alphanum[] =
+        "0123456789"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz";
+    
+    int i;
+    for ( i = 0; i < len; ++i) {
+        s[i] = alphanum[rand() % (sizeof(alphanum) - 1)];
+    }
+
+    s[len] = 0;
+}
