e8a2ee4b7972c03999c404736b196b76bde186ae
Vighnesh Birodkar <vighneshbirodkar@gmail.com>
2014-02-22 01:04:58 +0530

    speed test

--- tests/speed.py
+++ tests/speed.py
@@ -2,7 +2,7 @@ import socket
 import time
 import conf
 
-NUM = 10000
+NUM = 50000
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client_socket.connect(('localhost', conf.PORT))
